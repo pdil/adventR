fb56a01a43f4e1f3b8f54a130aaf6ced04f0cc10
pdil <dilorenzopl@gmail.com>
2015-12-01 19:49:09 -0500

    Upload basement file and update floors file

---
+++ day1/basement.R
@@ -0,0 +1,14 @@
+
+library(readr)
+# read input file
+input <- read_file("input")
+# split string into individual elements
+input_vec <- substring(input, 1:nchar(input), 1:nchar(input))
+# replace '(' with 1 and ')' with -1
+input_vec[input_vec == "("] <- "1"
+input_vec[input_vec == ")"] <- "-1"
+# convert to numbers and find cumulative sum
+input_vec <- as.numeric(input_vec)
+cumulative_floor <- cumsum(input_vec)
+# find first element where cumulative sum is negative
+which(cumulative_floor < 0)[1]
---
+++ day1/floors.R
@@ -0,0 +1,12 @@
+
+library(readr)
+# read input file
+input <- read_file("input")
+# remove all '('
+input_edit <- gsub("[(]", "", input)
+# compare before and after length (i.e. 'up' moves)
+num_up <- nchar(input) - nchar(input_edit)
+# the rest correspond to down moves
+num_down <- nchar(input) - num_up
+# result is number of up moves minus number of down moves
+num_up - num_down
