c37aebb715d8f57a66ab9786f75a0c99567354b9
pdil <dilorenzopl@gmail.com>
2015-12-05 16:24:08 -0500

    Upload day 5

---
+++ day5/1,2-nice.R
@@ -0,0 +1,9 @@
+
+library(readr)
+
+input <- read_lines("input.txt")
+sum(grepl("(.*[aeiou]){3}", input) & !grepl("(ab|cd|pq|xy)", input) & grepl("(.)\\1", input))
+#258
+
+sum(grepl("(..).*\\1", input, perl = TRUE) & grepl("(.).\\1", input))
+#53
